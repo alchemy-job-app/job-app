d6af32e5a36f4ea7ef7965ffb7df6060c5ccf1f8
Kevin Do <kevind0501@gmail.com>
2022-02-28 15:34:12 -0800

    revised getJobs function

--- src/services/jobs.js
+++ src/services/jobs.js
@@ -1,21 +1,21 @@
 import { client, parseData } from './client';
 
 export async function getJobs() {
-  const request = await client.from('jobs').select();
+  const request = await client.from('jobs').select().match({ user_id });
   return parseData(request);
 }
 
-export async function updateJob({ name, email, bio, birthday }) {
+export async function updateJob({ notes, deadline, company, position, completion }) {
   const request = await client
     .from('jobs')
-    .update({ name, bio, birthday })
-    .match({ email });
+    .update({ notes, deadline, company, position, completion  })
+    .match({ id });
   return parseData(request);
 }
 
-export async function createJob({ name, email, bio, birthday }) {
+export async function createJob({ notes, deadline, company, position, completion  }) {
   const request = await client
     .from('jobs')
-    .insert({ name, email, bio, birthday });
+    .insert({ notes, deadline, company, position, completion  });
   return parseData(request);
 }
