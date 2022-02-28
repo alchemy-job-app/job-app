b0d1023b35a5b97d7afa156f3d9f1d7fe89b920d
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-02-28 12:25:55 -0800

    adding color pallet to main

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   content: ['./src/**/*.{html,js,jsx}'],
   theme: {
     extend: {},
+    colors: {
+      queenpink: '#DBC2CF',
+      manatee: '#9FA2B2',
+      tealblue: '#3C7A89',
+      charcoal: '#2E4756',
+      gunmetal: '#16262E',
+      white: '#e2e8f0',
+    },
   },
   plugins: [],
 };
