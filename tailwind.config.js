4b6cb8f7e8db374feb7eee98f8b3766c86867c02
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-02-28 12:31:31 -0800

    adding supabase keys & client.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
-      queenpink: '#DBC2CF',
+      pink: '#DBC2CF',
       manatee: '#9FA2B2',
-      tealblue: '#3C7A89',
+      teal: '#3C7A89',
       charcoal: '#2E4756',
       gunmetal: '#16262E',
       white: '#e2e8f0',
