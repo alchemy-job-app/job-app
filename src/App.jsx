09e6ddfef0a819ddcbdd4ecb44cddb5d06a7e731
Kevin Do <kevind0501@gmail.com>
2022-02-28 16:59:05 -0800

    added the App.jsx browser router along with private routes and wrapped around the providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,48 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css'; /* Global CSS */
-import styles from './App.module.css'; /* CSS Modules */
+import { UserProvider } from './context/UserContext/UserContext';
+import { JobProvider } from './context/JobContext/JobContext'
+import Home from './views/Home/Home';
+import Auth from './views/Auth/Auth';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import Profile from './views/Profile/Profile';
+import JobCard from './views/JobCard/JobCard';
+import CreateEdit from './views/CreateEdit/CreateEdit';
+import About from './views/About/About';
 
 export default function App() {
   return (
-    <h1
-      className={`
-      bg-green-400
-        text-3xl
-        text-center
-      text-white
-        font-bold
-        p-10
-        w-1/2
-        mx-auto
-        mt-10
-        ${styles.myCustomCssClass}
-      `}
-    >
-      Hello, World!
-    </h1>
+    <UserProvider>
+      <JobProvider>
+        <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/sign-in">
+            <Auth />
+          </Route>
+          <Route exact path="/sign-up">
+            <Auth isSigningUp />
+          </Route>
+          <PrivateRoute exact path="/profile">
+            <Profile />
+          </PrivateRoute>
+          <PrivateRoute exact path="/profile/:id">
+            <JobCard />
+          </PrivateRoute>
+          <PrivateRoute exact path="/profile/:id/edit">
+            <CreateEdit isEditing/>
+          </PrivateRoute>
+          <PrivateRoute exact path="/profile/create">
+            <CreateEdit />
+          </PrivateRoute>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+        </BrowserRouter>
+      </JobProvider>
+    </UserProvider>
   );
 }
