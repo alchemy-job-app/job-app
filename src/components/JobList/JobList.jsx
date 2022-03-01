6388a81a688807800cce77711b5da97337604f54
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2022-03-01 16:23:25 -0700

    Begin JobList, write add job btn

--- src/components/JobList/JobList.jsx
+++ src/components/JobList/JobList.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+// import React from 'react';
+// import { useJob } from '../../context/JobContext/JobContext';
+// import JobCard from '../../views/JobCard/JobCard';
 
-export default function JobList() {
-  return (
-    <div>JobList</div>
-  )
-}
+// export default function JobList() {
+//   const { jobs, loading } = useJob();
+
+//   return (
+//     <div>
+//       <ul>
+//         {jobs.map((job) => (
+//           <li key={job.id}>
+//             <JobCard job={job} />
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import JobList from '../../components/JobList/JobList';
 import { useUser } from '../../context/UserContext/UserContext';
 
 export default function Profile() {
-  const { user } = useUser();
-  return <div>Profile</div>;
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/profile/create');
+  };
+
+  return (
+    <div className="bg-gunmetal h-screen w-full">
+      <button
+        onClick={handleClick}
+        className="bg-pink text-gunmetal mr-0 p-1 rounded-md hover:bg-manatee"
+      >
+        Add a Job!
+      </button>
+      {/* Your Jobs:
+      <JobList /> */}
+    </div>
+  );
 }
 
 // STRETCH GOAL:
