267f3a756ee0eee1ce05d816b20084162699d628
Kevin Do <kevind0501@gmail.com>
2022-03-01 11:21:56 -0800

    working sign up, sign in, home button on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -13,7 +13,9 @@ export default function Header() {
           <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
         </svg>
         <span className="font-semibold text-3xl tracking-tight">
-          Good<span className="text-pink">Job</span>
+          <a href="/">
+            Good<span className="text-pink">Job</span>
+          </a>
         </span>
       </div>
       <div className="block lg:hidden">
@@ -44,30 +46,33 @@ export default function Header() {
       <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto sm:hidden">
         <div className="text-sm lg:flex-grow">
           <a
-            href="#responsive-header"
+            href="#"
             className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
           >
             Interview Tips
           </a>
           <a
-            href="#responsive-header"
+            href="#"
             className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
           >
             Resume
           </a>
           <a
-            href="#responsive-header"
-            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+            href="#"
+            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
           >
             Personal
           </a>
-          <p>
-            <Link to="/about"> About Us</Link>
-          </p>
+          <a
+            href="/about"
+            className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
+          >
+            About Us
+          </a>
         </div>
         <div>
           <a
-            href="#"
+            href="/sign-in"
             className="inline-block text-sm px-4 py-2 leading-none border rounded text-gunmetal border-gunmetal hover:border-transparent bg-white hover:text-white hover:bg-gunmetal mt-4 lg:mt-0"
           >
             Sign In
@@ -75,7 +80,7 @@ export default function Header() {
         </div>
         <div>
           <a
-            href="#"
+            href="/sign-up"
             className="inline-block text-sm px-4 py-2 ml-4 leading-none border rounded text-white border-gunmetal hover:border-transparent bg-gunmetal hover:text-gunmetal hover:bg-white mt-4 lg:mt-0"
           >
             Sign Up
