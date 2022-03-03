2dff1b8baee985e3599076a8a443105820497ba2
Kevin Do <kevind0501@gmail.com>
2022-03-02 17:31:04 -0800

    worked on banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 export default function Banner() {
   return (
-    <div className="relative w-full overflow-hidden">
-      <img
-        src="https://reviverestore.org/wp-content/uploads/2018/11/ocean-banner.jpg"
-        alt="Avatar"
-        class="object-cover w-full h-full"
-      />
-      <div className="absolute w-full py-2.5 bottom-0 inset-x-0 bg-blue-400 text-white text-3xl text-center leading-4">
-        this is a text
+    <div className="flex items-stretch text-white bg-white">
+      <div className="lg:flex relative bg-no-repeat bg-cover items-center bg-banner bg-center h-96">
+        <div className="w-screen px-24 z-10">
+          <h1 className="text-5xl font-bold text-left text-whitewhite">
+            Get that job!
+          </h1>
+          <p className="text-3xl my-4 text-white">
+            Organize and store your job applications
+          </p>
+        </div>
       </div>
     </div>
   );
